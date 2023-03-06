6d5a779bf47a9ab1a627dd13ec8052c1476bda2c
abhay <abhaysp95@protonmail.com>
2023-03-06 20:02:18 +0530

    feat: get the input as regex object

--- findr/src/lib.rs
+++ findr/src/lib.rs
@@ -1,7 +1,9 @@
-use std::{error::Error, io};
+use std::{error::Error, fs::DirEntry};
 
 use clap::{command, Parser, ValueEnum};
 use regex::Regex;
+
+// TODO: switch to Builder pattern (possibly, derive pattern not working)
 
 #[derive(PartialEq, Eq, Debug, Clone, ValueEnum)]
 enum FindType {
@@ -17,14 +19,14 @@ enum FindType {
 #[command(about, version, long_about=None)]
 /// simple rust clone of find
 pub struct Config {
-    /// pathes to find on
+    /// paths to find on
     #[arg()]
-    path: Vec<String>,
-    #[arg(short='t', long="type", value_enum)]
+    paths: Vec<String>,
+    #[arg(short = 't', long = "type", value_enum)]
     ftype: Vec<FindType>,
     /// pattern to find
-    #[arg(long)]
-    name: Vec<String>,
+    #[arg(long = "name")]
+    names: Vec<String>,
 }
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
@@ -32,26 +34,30 @@ type MyResult<T> = Result<T, Box<dyn Error>>;
 pub fn parse_args() -> MyResult<Config> {
     let mut cfg = Config::parse();
 
-    if cfg.path.is_empty() {
-        cfg.path = vec![String::from(".")];
+    if cfg.paths.is_empty() {
+        cfg.paths = vec![String::from(".")];
     }
 
     Ok(cfg)
 }
 
 pub fn run(cfg: Config) -> MyResult<()> {
-    dbg!(&cfg);  // will show everything escaped (but actually it is not)
+    dbg!(&cfg); // will show everything escaped (but actually it is not)
 
-    println!("{:?}", cfg.name);
+    #[allow(unused_variables)]
+    let names = cfg
+        .names
+        .iter()
+        .map(|name| Regex::new(&name).map_err(|_| format!("Invalid --name \"{}\"", name)))
+        .collect::<Result<Vec<_>, _>>()?;
 
-    let mut to_regex = String::from("");
-
-
-    println!("{}", to_regex);
+    for path in cfg.paths {
+    }
 
     Ok(())
 }
 
+#[allow(dead_code)]
 fn regex_do_match(haystack: &str, needle: &str) -> bool {
     let reg = Regex::new(needle).unwrap();
     reg.is_match(&haystack)
