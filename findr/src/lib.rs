eb455ec2c6ae8757fcf4c3b1ab3352972d83d3dd
abhay <abhaysp95@protonmail.com>
2023-03-07 13:28:26 +0530

    feat: use builder for clap and update parse_args

--- findr/src/lib.rs
+++ findr/src/lib.rs
@@ -1,56 +1,85 @@
-use std::{error::Error, fs::DirEntry};
+use std::{error::Error, path::PathBuf};
 
-use clap::{command, Parser, ValueEnum};
+use clap::{arg, value_parser, Arg, ArgAction, Command};
 use regex::Regex;
 use walkdir::WalkDir;
 
 // TODO: switch to Builder pattern (possibly, derive pattern not working)
 
-#[derive(PartialEq, Eq, Debug, Clone, ValueEnum)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 enum FindType {
-    /// search directories
     Dir,
-    /// search files
     File,
-    /// search symlinks
     Link,
 }
 
-#[derive(Parser, Debug)]
-#[command(about, version, long_about=None)]
-/// simple rust clone of find
-pub struct Config {
-    /// paths to find on
-    #[arg()]
+#[derive(Debug)]
+struct Config {
     paths: Vec<String>,
-    #[arg(short = 't', long = "type", value_enum)]
-    ftype: Vec<FindType>,
-    /// pattern to find
-    #[arg(long = "name")]
-    names: Vec<String>,
+    ftypes: Vec<FindType>,
+    names: Vec<Regex>,
 }
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 pub fn parse_args() -> MyResult<Config> {
-    let mut cfg = Config::parse();
+    let matches = Command::new("findr")
+        .version("0.1.0")
+        .author("coolabhays")
+        .about("simple clone of find in rust")
+        .arg(
+            Arg::new("paths")
+                .required(true)
+                .value_parser(value_parser!(PathBuf))
+                .action(ArgAction::Append)
+                .help("Provide paths to search"),
+        )
+        .arg(
+            arg!(-t --type <TYPE> "match type of the file")
+                .action(ArgAction::Append)
+                .value_parser(["d", "f", "l"])
+                .default_value("f")
+                .help("Types of file to search"),
+        )
+        .arg(
+            arg!(-n --name <NAME> "match the pattern for the result")
+                .action(ArgAction::Append)
+                .help("Patterns to search"),
+        )
+        .get_matches();
 
-    if cfg.paths.is_empty() {
-        cfg.paths = vec![String::from(".")];
-    }
+    let paths = matches
+        .get_many::<String>("paths")
+        .unwrap_or_default()
+        .map(|path| path.clone()) // why do this ?
+        .collect::<Vec<String>>();
 
-    Ok(cfg)
+    let ftypes = matches
+        .get_many::<String>("type")
+        .unwrap_or_default()
+        .map(|t| t.as_str())
+        .map(|t| match t {
+            "d" => FindType::Dir,
+            "f" => FindType::File,
+            "l" => FindType::Link,
+        })
+        .collect::<Vec<FindType>>();
+
+    let names = matches
+        .get_many::<String>("name")
+        .unwrap_or_default()
+        .map(move |n| Regex::new(n).unwrap())
+        .collect::<Vec<Regex>>();
+
+    Ok(Config {
+        paths,
+        ftypes,
+        names,
+    })
 }
 
 pub fn run(cfg: Config) -> MyResult<()> {
     dbg!(&cfg); // will show everything escaped (but actually it is not)
-
-    #[allow(unused_variables)]
-    let names = cfg
-        .names
-        .iter()
-        .map(|name| Regex::new(&name).map_err(|_| format!("Invalid --name \"{}\"", name)))
-        .collect::<Result<Vec<_>, _>>()?;
 
     let mut find_res: Vec<String> = vec![];
 
@@ -60,23 +89,23 @@ pub fn run(cfg: Config) -> MyResult<()> {
                 Err(e) => eprintln!("{}", e),
                 Ok(entry) => {
                     let ft = entry.file_type();
-                    for ftype in &cfg.ftype {
+                    for ftype in &cfg.ftypes {
                         match ftype {
                             FindType::File => {
                                 if ft.is_file() {
                                     find_res.push(entry.path().display().to_string());
                                 }
-                            },
+                            }
                             FindType::Dir => {
                                 if ft.is_dir() {
                                     find_res.push(entry.path().display().to_string());
                                 }
-                            },
+                            }
                             FindType::Link => {
                                 if ft.is_symlink() {
                                     find_res.push(entry.path().display().to_string());
                                 }
-                            },
+                            }
                         }
                     }
                 }
