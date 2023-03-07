b19f3308f7c1a8d192d5c8a2ccf9ceb73f52f688
abhay <abhaysp95@protonmail.com>
2023-03-07 17:27:43 +0530

    feat: using closures and making code not so much shift right

--- findr/src/lib.rs
+++ findr/src/lib.rs
@@ -77,28 +77,39 @@ pub fn parse_args() -> MyResult<Config> {
 }
 
 pub fn run(cfg: Config) -> MyResult<()> {
-    for path in cfg.paths {
-        for entry in WalkDir::new(&path) {
-            match entry {
-                Err(e) => eprintln!("{}", e),
-                Ok(entry) => {
-                    if (cfg.ftypes.is_empty()
-                        || cfg.ftypes.iter().any(|ft| match ft {
-                            FindType::File => entry.file_type().is_file(),
-                            FindType::Dir => entry.file_type().is_dir(),
-                            FindType::Link => entry.file_type().is_symlink(),
-                        }))
-                        && (cfg.names.is_empty()
-                            || cfg
-                                .names
-                                .iter()
-                                .any(|name| name.is_match(entry.file_name().to_str().unwrap())))
-                    {
-                        println!("{}", entry.path().display());
-                    }
+    let type_filter = |entry: &DirEntry| {
+        cfg.ftypes.is_empty()
+            || cfg.ftypes.iter().any(|ft| match ft {
+                FindType::Link => entry.file_type().is_symlink(),
+                FindType::Dir => entry.file_type().is_dir(),
+                FindType::File => entry.file_type().is_file(),
+            })
+    };
+
+    let name_filter = |entry: &DirEntry| {
+        return cfg.names.is_empty()
+            || cfg
+                .names
+                .iter()
+                .any(|name| name.is_match(&entry.file_name().to_string_lossy()));
+    };
+
+    for path in &cfg.paths {
+        let entries = WalkDir::new(&path)
+            .into_iter()
+            .filter_map(|entry| match entry {
+                Err(e) => {
+                    println!("{}", e);
+                    None
                 }
-            }
-        }
+                Ok(entry) => Some(entry),
+            })
+            .filter(type_filter)
+            .filter(name_filter)
+            .map(|entry| entry.path().display().to_string())
+            .collect::<Vec<String>>();
+
+        println!("{}", entries.join("\n"));
     }
 
     Ok(())
